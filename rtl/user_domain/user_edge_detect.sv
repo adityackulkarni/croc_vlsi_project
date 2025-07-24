e04d60f0331082f920eddd4585246584660e4655
aditya <adityackulkarni24@gmail.com>
2025-07-24 14:35:05 +0200

    made similar to rom

--- rtl/user_domain/user_edge_detect.sv
+++ rtl/user_domain/user_edge_detect.sv
@@ -27,34 +27,55 @@ module user_edge_detect #(
 
 
   // Define some registers to hold the requests fields
-  logic req_d, req_q;
-  logic we_d, we_q;
-  logic [ObiCfg.AddrWidth-1:0] addr_d, addr_q;
-  logic [ObiCfg.IdWidth-1:0] id_d, id_q;
-  logic [ObiCfg.DataWidth-1:0] wdata_d, wdata_q;
+  logic req_d, req_q, req_q2;
+  logic we_d, we_q, we_q2;
+  logic [ObiCfg.AddrWidth-1:0] addr_d, addr_q, addr_q2;
+  logic [ObiCfg.IdWidth-1:0] id_d, id_q, id_q2;
+  logic [ObiCfg.DataWidth-1:0] wdata_d, wdata_q, wdata_q2;
 
   // Signals used to create the response
   logic [ObiCfg.DataWidth-1:0] rsp_data; // Data field of the obi response
   logic rsp_err; // Error field of the obi response
 
   // Internal signals/registers
-  logic [15:0] set_bits_accumulator_d, set_bits_accumulator_q; // Holding the accumulated bitcount
+  logic [15:0] set_bits_accumulator_d, set_bits_accumulator_q, set_bits_accumulator_q2; // Holding the accumulated bitcount
   logic [15:0] wdata_cnt; // Olding the bitcount of the request wdata
   
   // Note to avoid writing trivial always_ff statements we can use this macro defined in registers.svh 
-  `FF(req_q, req_d, '0);
-  `FF(id_q , id_d , '0);
-  `FF(we_q , we_d , '0);
-  `FF(wdata_q , wdata_d , '0);
-  `FF(addr_q , addr_d , '0);
-  `FF(set_bits_accumulator_q, set_bits_accumulator_d, '0);
-
   assign req_d = obi_req_i.req;
   assign id_d = obi_req_i.a.aid;
   assign we_d = obi_req_i.a.we;
   assign addr_d = obi_req_i.a.addr;
   assign wdata_d = obi_req_i.a.wdata;
-
+  always_ff @(posedge (clk_i) or negedge (rst_ni)) begin
+    if (!rst_ni) begin
+      req_q <= '0;
+      id_q <= '0;
+      we_q <= '0;
+      addr_q <= '0;
+      wdata_q <= '0;
+      set_bits_accumulator_q <= '0;
+      req_q2 <= '0;
+      id_q2 <= '0;
+      we_q2 <= '0;
+      addr_q2 <= '0;
+      wdata_q2 <= '0;
+      set_bits_accumulator_q2 <= '0;
+    end else begin
+      req_q <= req_d;
+      id_q <= id_d;
+      we_q <= we_d;
+      addr_q <= addr_d;
+      wdata_q <= wdata_d;
+      set_bits_accumulator_q <= set_bits_accumulator_d;
+      req_q2 <= req_q;
+      id_q2 <= id_q;
+      we_q2 <= we_q;
+      addr_q2 <= addr_q;
+      wdata_q2 <= wdata_q;
+      set_bits_accumulator_q2 <= set_bits_accumulator_q;
+    end
+  end
 
   // TODO 2: Build wdata_cnt, which counts the number of bits set in the previous request's data.
   
@@ -62,7 +83,7 @@ module user_edge_detect #(
   begin 
     wdata_cnt = 0; 
     for (int i = 0; i < 32 ; i++ ) 
-      if(wdata_q[i]) wdata_cnt += 1;  
+      if(wdata_q2[i]) wdata_cnt += 1;  
   end
 
   // Assign the response data
@@ -70,32 +91,32 @@ module user_edge_detect #(
   always_comb begin
     rsp_data = '0;
     rsp_err  = '0;
-    word_addr = addr_q[3:2];
-    set_bits_accumulator_d = set_bits_accumulator_q;
+    word_addr = addr_q2[3:2];
+    set_bits_accumulator_d = set_bits_accumulator_q2;
 
     // TODO 1: A write request at address 0x0 will set the accumulator to zero
 
-    if(req_q) begin
+    if(req_q2) begin
       case(word_addr)
         3'h0: begin
-          if(we_q) begin
+          if(we_q2) begin
             set_bits_accumulator_d = '0;
           end else begin
             rsp_err = '1;
           end
         end
         3'h1: begin
-          if(we_q) begin
-            set_bits_accumulator_d = set_bits_accumulator_q + wdata_cnt;
+          if(we_q2) begin
+            set_bits_accumulator_d = set_bits_accumulator_q2 + wdata_cnt;
           end else begin
             rsp_err = '1;
           end
         end
         3'h2: begin
-          if(we_q) begin
+          if(we_q2) begin
             rsp_err = '1;
           end else begin
-            rsp_data = set_bits_accumulator_q;
+            rsp_data = set_bits_accumulator_q2;
           end
         end
         default: rsp_data = 32'hffffffff;
@@ -107,9 +128,9 @@ module user_edge_detect #(
   // A channel
   assign obi_rsp_o.gnt = obi_req_i.req;
   // R channel:
-  assign obi_rsp_o.rvalid = req_q;
+  assign obi_rsp_o.rvalid = req_q2;
   assign obi_rsp_o.r.rdata = rsp_data;
-  assign obi_rsp_o.r.rid = id_q;
+  assign obi_rsp_o.r.rid = id_q2;
   assign obi_rsp_o.r.err = rsp_err;
   assign obi_rsp_o.r.r_optional = '0;
 
